391a403ac50f1ba21cf3dc7c773ea077a4c35113
Romny <romnyurielcalona@gmail.com>
2023-08-19 21:56:07 -0600

    Comienzo del proyeco

---
+++ 01-funciones.py
@@ -0,0 +1,20 @@
+
+# definicion de funciones
+def hola():
+    print("Hello world")
+    print("Ultimate Python")
+
+
+hola()
+
+# se agrega un valor por defecto en algun parametro, en caso de si enviar un valor toma el otro
+
+
+def hola2(nombre, apellido="Shurman"):
+    print(f"Bienvenido : {nombre} {apellido}")
+
+
+hola2("Nicolas", "Blaz")
+
+# se llama la funcion sin importar el orden de los argumentos, pero definiendo a que corresponde cada uno
+hola2(apellido="Shurman", nombre="Nicolas")
